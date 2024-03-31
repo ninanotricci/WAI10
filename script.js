37fbe5c9ca0face83c439e98d545ccd495ee647d
Nina <tsikaridze@outlook.com>
2024-03-31 17:39:48 +0400

    add Js

---
+++ script.js
@@ -0,0 +1,35 @@
+// დავალება 1
+// შექმენით პროგრამა, რომელიც გამოთვლის
+// მართკუთხედის ფართობს. ცვლადში უნდა
+// განსაზღვროთ მართკუთხედის სიგრძე და სიგანე და
+// ლოგიკა გამოიტანოთ ცვლადში result. და ბოლოს
+// კონსოლ ლოგში გამოიტანეთ result.
+
+let oneSide = 2;
+let secondSide = 6;
+
+area = oneSide * secondSide;
+console.log(area);
+
+// დავალება 2
+// განსაზღვრეთ ორი ცვლადი (num1 და num2) რიცხვითი მნიშვნელობებით,
+// რომლებიც წარმოადგენენ ციფრებს, რომლებზეც უნდა იმოქმედოთ.
+// გამოიყენეთ მატემატიკური ოპერატორები, შემდეგი გამოთვლების
+// შესასრულებლად(მიმატება, გამოკლება, გამრავლება, გაყოფა)
+// შეინახეთ თითოეული ოპერაციის შედეგები ცალკეულ
+// ცვლადებში(AdditionResult,SubstractionResult და ა.შ).
+// დააკონსოლეთ თითოეული შედეგი.
+
+let num1 = 15;
+let num2 = 10;
+
+AdditionResult = num1 + num2;
+SubstractionResult = num1 - num2;
+anotherResult = num1 * num2;
+oneAnotherResoult = num1 / num2;
+console.log(
+  AdditionResult,
+  SubstractionResult,
+  anotherResult,
+  oneAnotherResoult
+);
